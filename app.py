467016004e3a5ef04cdeaf66a2c8b402f02aac0e
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-08-03 20:56:23 -0600

    Finished first draft of gallery

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-  print("Get request string")
-  return render_template('index.html')
+@app.route("/")
+def gallery():
+    image_files = os.listdir("static/images")  # Assuming your images are in the static/images directory
+    return render_template("gallery.html", image_files=image_files)
 
-
-@app.route('/', methods = ['POST'])
-def home_post():
-  dim1 = request.form['first_dim']
-  dim2 = request.form['second_dim']
-  dim3 = request.form['third_dim']
-  volume = float(dim1) * float(dim2) * float(dim3)
-  print()
-
-  print("Get post request")
-  return render_template('index.html', output=volume, dim_1=dim1, dim_2=dim2, dim_3=dim3)
-
-app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
